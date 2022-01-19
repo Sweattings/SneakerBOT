9094f8a111adf80f7e5a12289158a1432a86285a
Sweattings <philippfritz808@yahoo.de>
2022-01-19 19:03:38 +0100

    new chromedriver

--- bot.py
+++ bot.py
@@ -1,26 +1,45 @@
-import time
-from selenium.webdriver.chrome.options import Options
-from xml.dom.minidom import Element
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+try :
+    from lib2to3.pgen2 import driver
+    import time
+    from selenium.webdriver.chrome.options import Options
+    from xml.dom.minidom import Element
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+    import undetected_chromedriver.v2 as uc
+    print('all module are loaded ')
+
+except Exception as e:
+
+    print("Error ->>>: {} ".format(e))
+
+
+#driver = webdriver.Chrome('chromedriver.exe', options=options)
+driver = uc.Chrome(use_subprocess=True)
 
 # Config
 options = Options()
 options = webdriver.ChromeOptions()
-# Webdriver
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome('chromedriver.exe', options=options)
 
+
+
+
+
+"""
 #For ChromeDriver version 79.0.3945.16 or over
 options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_argument('--no-sandbox')
+options.add_argument('--single-process')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--incognito")
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_experimental_option('useAutomationExtension', False)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_argument("disable-infobars")
+"""
 
-# Headless
-options.headless = True
-#options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+
 
 # Url Selection
 url = "https://omi.veve.me/";
@@ -33,15 +52,14 @@ time.sleep(1)
 email = driver.find_element_by_xpath(em_xp)
 email.click()
 email.send_keys("philipp.fritz4@gmail.com")
-time.sleep(3)
-
+	
 
 # Passwort Selctor
 pw_xp = ("//input[@name = 'password']")
-time.sleep(3)
+
 Passwd = driver.find_element_by_xpath(pw_xp)
 Passwd.send_keys("1Qay2wsx3edc!")
-time.sleep(2)
+
 
 
 # Press login Button
@@ -51,7 +69,7 @@ login = driver.find_element_by_xpath(lg_xp)
 login.click()
 
 
-
-
 time.sleep(5)
+
+
 driver.quit()
